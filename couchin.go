4311d2e402ed2709dd757de7e6a73eb889313a33
Lance Carlson <lancecarlson@gmail.com>
2013-05-13 00:36:18 -0400

    uses channels

--- couchin.go
+++ couchin.go
@@ -9,6 +9,7 @@ import (
 	"encoding/base64"
 	"net/http"
 	"strings"
+//	"time"
 )
 
 func Partition(list []string, size int, f func([]string)) {
@@ -16,7 +17,7 @@ func Partition(list []string, size int, f func([]string)) {
 		if i+size > len(list) {
 			f(list[i:])
 		} else {
-			f(list[i : i+size])
+			f(list[i:i+size])
 		}
 	}
 }
@@ -50,18 +51,26 @@ func SaveDocs(docs []string, saveUrl string) (*http.Response, error) {
 	return http.Post(saveUrl, "application/json", strings.NewReader(body))
 }
 
-func PartitionHandler(keys []string, client *redis.Client, saveUrl *string) {
+func Work(keys []string, client *redis.Client, saveUrl *string) (string, error) {
 	docs := GetDocs(keys, client)
 	resp, err := SaveDocs(docs, *saveUrl)
-	if err != nil {
-		log.Println(err)
-	} else {
-		body, err := ioutil.ReadAll(resp.Body)
+	if err != nil { return "", err }
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil { return "", err }
+	return string(body), nil
+}
+
+func Worker(id int, client *redis.Client, saveUrl *string, jobs <- chan []string, results chan<- string) {
+	for j := range jobs {
+		log.Println("worker", id, "processing job")
+		log.Println(len(j))
+		body, err := Work(j, client, saveUrl)
 		if err != nil {
-			log.Print("Error: ")
 			log.Println(err)
+			results <- ""
 		} else {
-			log.Println(string(body))
+			log.Println(body)
+			results <- body
 		}
 	}
 }
@@ -72,7 +81,8 @@ func main() {
 	db := flag.Int64("db", 0, "select the Redis db integer")
 	saveUrl := flag.String("save-url", "", "bulk save url. ie: http://localhost:5984/db/_bulk_docs")
 	saveLimit := flag.Int("save-limit", 1000, "number of docs to save at once")
-	flush := flag.Bool("flush", true, "flush Redis after finished")
+	workerCount := flag.Int("workers", 20, "number of works to batch save")
+	flush := flag.Bool("flush", false, "flush Redis after finished")
 	flag.Parse()
 	client := redis.NewTCPClient(*host, *password, *db)
 	defer client.Close()
@@ -82,9 +92,28 @@ func main() {
 	if keys.Err() != nil {
 		log.Fatal(keys.Err())
 	} else {
+		keyCount := len(keys.Val())
+		jobCount := int(keyCount/(*saveLimit))+1
+		if keyCount <= 0 {
+			log.Fatal("No Keys")
+		}
+		jobs := make(chan []string, jobCount)
+		results := make(chan string, jobCount)
+
+		for w := 1; w <= *workerCount; w++ {
+			go Worker(w, client, saveUrl, jobs, results)
+		}
+
 		Partition(keys.Val(), *saveLimit, func(keys []string) {
-			PartitionHandler(keys, client, saveUrl)
+			jobs <- keys
 		})
+
+		close(jobs)
+
+		for a := 1; a <= jobCount; a++ {
+			<-results
+		}
+
 		if *flush {
 			log.Println("Flushing...")
 			client.FlushDb()
